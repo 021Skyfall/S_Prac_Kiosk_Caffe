6d657e1023360dfac9333c50e827fd88c50b5a94
021Skyfall <021skyfall@gmail.com>
2022-12-31 21:37:54 +0900

    카페 키오스크 .v1

--- Discount/BEDiscountCondition.java
+++ Discount/BEDiscountCondition.java
@@ -1,32 +1,29 @@
-//package Discount;
-//
-//import java.util.Scanner;
-//
-//public class BEDiscountCondition {
-//    private int discountAmount;
-//
-//    public BEDiscountCondition(int discountAmount) {
-//        this.discountAmount = discountAmount -20/100;
-//    }
-//    // 할인 조건을 물어보는 메소드
-//    private boolean checkDiscountCondition() {
-//        Scanner scanner = new Scanner(System.in);
-//
-//        System.out.println("학생이십니까? (1)_예 (2)_아니오");
-//        String input = scanner.nextLine();
-//
-//        if(input.equals("1")) return true;
-//        else return false;
-//    }
-//    // 할인을 적용시키는 메소드
-//    private int calPrice(int price) {
-//        return price - discountAmount;
-//    }
-//    public int discount(int price) {
-//        if (checkDiscountCondition()) return calPrice(price);
-//        else return price;
-//    }
-//}
+package Discount;
 
+import java.util.Scanner;
 
-//리퍼런스 자료 보고 고쳐보자
+public class BEDiscountCondition implements DiscountCondition{
+    private int discountAmount;
+
+    public BEDiscountCondition(int discountAmount) {
+        this.discountAmount = discountAmount;
+    }
+    // 할인 조건을 물어보는 메소드
+    private boolean checkDiscountCondition() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("학생이십니까? (1)_예 (2)_아니오");
+        String input = scanner.nextLine();
+
+        if(input.equals("1")) return true;
+        else return false;
+    }
+    // 할인을 적용시키는 메소드
+    private int calPrice(int price) {
+        return price - (price * discountAmount / 100);
+    }
+    public int discount(int price) {
+        if (checkDiscountCondition()) return calPrice(price);
+        else return price;
+    }
+}
